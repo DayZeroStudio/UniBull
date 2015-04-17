2f6046e2d21696be47298d64aa9369feffd66e6d
Evan Hughes <eshughes@ucsc.edu>
2015-04-17 15:08:47 -0700

    added classroom html file (bare bones inside), signup.js, changes to login and signup html

---
+++ views/signup.js
@@ -0,0 +1,33 @@
+module.exports = (function() {
+    "use strict";
+    var exports = {};
+
+    exports.onSignUp = function($, window, fields) {
+        var formData = {};
+        fields.each(function(i, v) {
+            console.log(v);
+            console.log($(v).attr("name"));
+            var name = $(v).attr("name");
+            formData[name] = $(v).val();
+        });
+        console.log(formData);
+        $.ajax({
+            type: "POST",
+            url: "rest/user/signup",
+            data: JSON.stringify(formData),
+            error: function(res, textStatus, errorThrown) {
+                console.log("res:" + res.responseText);
+                console.log("error: " + errorThrown);
+            },
+            success: function(data) {
+                console.log("data: ", data);
+                window.location.replace(data.redirect);
+            },
+            dataType: "json",
+            contentType: "application/json"
+        });
+        return false;
+    };
+
+    return exports;
+})();
