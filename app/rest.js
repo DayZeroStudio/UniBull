ac257085f0e993b3c017b9b170a00fc9210f4fe1
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2015-04-25 02:04:54 -0700

    app/rest now uses fs with promises

--- app/rest.js
+++ app/rest.js
@@ -2,9 +2,8 @@
 var Promise = require("sequelize").Promise;
 
 module.exports = Promise.promisify(function setupRestEndpoints(models, done) {
-    var fs = require("fs");
-    var _ = require("lodash");
-    var async = require("async");
+    var fs = Promise.promisifyAll(require("fs"));
+
     var cfg = require("../config");
     var log = cfg.log.logger;
 
@@ -12,17 +11,14 @@ module.exports = Promise.promisify(function setupRestEndpoints(models, done) {
     var appRouter = express.Router();
 
     // Automagically app.use all rest/*.js
-    fs.readdir("rest", function(err, files) {
-        if (err) {throw err; }
-        log.info("files:", files);
-        async.each(files, function(file, eachCallback) {
-            var filePath = "../rest/" + file;
-            var route = "/rest/" + file.substr(0, file.indexOf("."));
-            log.info("Adding REST endpoint: (" + route + ")");
-            require(filePath)(models, route).then(function(router) {
-                appRouter.use(route, router);
-                eachCallback(null);
-            });
-        }, _.partial(done, _, appRouter));
+    fs.readdirAsync("rest").each(function(file) {
+        var requirePath = "../rest/" + file;
+        var route = "/rest/" + file.substr(0, file.indexOf("."));
+        log.info("Adding REST endpoint: (" + route + ")");
+        require(requirePath)(models, route).then(function(router) {
+            appRouter.use(route, router);
+        });
+    }).then(function() {
+        done(null, appRouter);
     });
 });
