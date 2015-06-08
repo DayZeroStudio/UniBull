6279c2ac85830c37836ad6bbda93da08b7578bbe
Evan Hughes <eshughes@ucsc.edu>
2015-06-07 17:09:07 -0700

    added endorsing of replies - tests incomplete

--- app/rest/reply.js
+++ app/rest/reply.js
@@ -272,5 +272,60 @@ module.exports = function(dbModels) {
             });
         });
     });
+
+    router.post("/:classID/thread/:threadID/reply/:replyID/endorse", function(req, res) {
+        log.info("POST - endorsing a reply");
+        var classID = req.params.classID;
+        var threadID = req.params.threadID;
+        var replyID = req.params.replyID;
+        var reason = req.body.reason;
+        Class.find({
+            where: {uuid: classID}
+        }).bind({}).then(function(klass) {
+            this.class = klass;
+        }).then(function() {
+            var decoded = auth.decodeRequest(req);
+            var username = decoded.username;
+            var instructorRoles = {
+                "ta": 1,
+                "professor": 2
+            };
+            if (instructorRoles.indexOf(decoded.role) < 0) {
+                throw Error(cfg.errmsgs.naughtyUser);
+            }
+            return User.find({
+                where: {username: username}
+            });
+        }).then(function(user) {
+            if (!user) {
+                throw Error(cfg.errmsgs.invalidUserInfo);
+            }
+            this.user = user;
+        }).then(function() {
+            return this.class.getThreads({
+                where: {uuid: threadID}
+            });
+        }).then(function(threads) {
+            this.thread = threads[0];
+            return this.thread.getReplies({
+                where: {uuid: replyID}
+            });
+        }).then(function(replies) {
+            var reply = replies[0];
+            if (this.user.uuid === reply.UserUuid) {
+                throw Error(cfg.errmsgs.sillyUser);
+            }
+            if (!reply.endorsed) {
+                reply.endorsed = [];
+            }
+            reply.endorsed.push(reason);
+            return reply.save();
+        }).then(function(reply) {
+            return res.json({
+                reply: reply
+            });
+        }).catch(cfg.handleErr(res));
+    });
+
     return Promise.resolve(router);
 };
