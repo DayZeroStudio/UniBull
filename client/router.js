d562a191980b2a23bc0d86dc6f43cb200379f825
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2015-08-30 19:56:11 -0700

    adding wrapper to router handlers to make sure we are logged in before trying to access a page

--- client/router.js
+++ client/router.js
@@ -12,6 +12,17 @@ var AddClassPage = require("./pages/add-class.js");
 var Classes = require("./models/classes.js");
 var Threads = require("./models/threads.js");
 
+function ensureLoggedIn(handler) {
+    return function() {
+        if (!app.user.username) {
+            window.history.replaceState("Login", "Login", "/login");
+            app.trigger("page", new LoginPage());
+            return;
+        }
+        handler.apply(this, arguments);
+    };
+}
+
 module.exports = Router.extend({
     routes: {
         "": "login",
@@ -25,9 +36,9 @@ module.exports = Router.extend({
     },
 
     // ------- ROUTE HANDLERS ---------
-    home: function() {
+    home: ensureLoggedIn(function() {
         app.trigger("page", new HomePage());
-    },
+    }),
 
     login: function() {
         app.trigger("page", new LoginPage());
@@ -37,28 +48,28 @@ module.exports = Router.extend({
         app.trigger("page", new SignupPage());
     },
 
-    classes: function() {
+    classes: ensureLoggedIn(function() {
         if (!app.allClasses) {
             app.allClasses = new Classes();
         }
         app.allClasses.fetch();
         app.trigger("page", new ClassesPage());
-    },
+    }),
 
-    addClass: function() {
+    addClass: ensureLoggedIn(function() {
         app.trigger("page", new AddClassPage());
-    },
+    }),
 
-    class: function(uuid) {
+    class: ensureLoggedIn(function(uuid) {
         var threads = new Threads({uuid: uuid});
         threads.fetch();
         app.trigger("page", new ClassPage({
             model: app.class,
             collection: threads
         }));
-    },
+    }),
 
-    catchAll: function() {
+    catchAll: ensureLoggedIn(function() {
         app.trigger("page", new HomePage());
-    }
+    })
 });
